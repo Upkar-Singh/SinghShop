ae876ada72b6a40398a282e6f8f157b300b54faa
upkar-singh <uupkarssingh@gmail.com>
2019-09-19 15:16:46 -0400

    Finished methods to add/subtract quantity.

--- app/src/main/java/com/example/singhshop/MenuActivity.java
+++ app/src/main/java/com/example/singhshop/MenuActivity.java
@@ -4,9 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MenuActivity extends AppCompatActivity {
+
+    private int mCount;
+    private double newTotal;
+    private double price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,36 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void addItem(View view) {
-        findViewById(R.this.addButton);
+        Button addBut = findViewById(R.id.add_button);
+      //  Log.d("addMenu", addBut.getText().toString());
+        ViewGroup cardParentView = (ViewGroup) addBut.getParent().getParent();
+        TextView priceTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(3)).getChildAt(1);
+        TextView subTotalTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(4)).getChildAt(3);
+        TextView quantityTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(3)).getChildAt(3);
+        mCount = Integer.parseInt(quantityTextView.getText().toString()) + 1;
+        price = Double.parseDouble(priceTextView.getText().toString());
+        newTotal = price * mCount;
+        subTotalTextView.setText(String.format("$%.2f", newTotal));
+        quantityTextView.setText(String.format("%d", mCount));
 
+        //Log.d("subtotal=", priceTextView.getText().toString());
+
+    }
+
+    public void removeItem(View view) {
+        Button addBut = findViewById(R.id.minus_button);
+        //  Log.d("addMenu", addBut.getText().toString());
+        if (mCount > 0) {
+            ViewGroup cardParentView = (ViewGroup) addBut.getParent().getParent();
+            TextView priceTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(3)).getChildAt(1);
+            TextView subTotalTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(4)).getChildAt(3);
+            TextView quantityTextView = (TextView) ((ViewGroup) cardParentView.getChildAt(3)).getChildAt(3);
+            mCount = Integer.parseInt(quantityTextView.getText().toString()) - 1;
+            price = Double.parseDouble(priceTextView.getText().toString());
+            newTotal = price * mCount;
+            subTotalTextView.setText(String.format("$%.2f", newTotal));
+            quantityTextView.setText(String.format("%d", mCount));
+        }
+        //Log.d("subtotal=", priceTextView.getText().toString());
     }
 }
